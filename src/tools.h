72f8aeb3fd14f1294b7959e573d1e25008fd204f
IbrahimOmar91 <46249457+IbrahimOmar91@users.noreply.github.com>
2019-03-14 23:12:51 +0200

    Add files via upload

--- src/tools.h
+++ src/tools.h
@@ -1,32 +1,40 @@
 #ifndef TOOLS_H_
 #define TOOLS_H_
-
 #include <vector>
 #include "Eigen/Dense"
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
 class Tools {
- public:
+public:
   /**
-   * Constructor.
-   */
+  * Constructor.
+  */
   Tools();
+  Tools(const Tools &);
 
   /**
-   * Destructor.
-   */
+  * Destructor.
+  */
   virtual ~Tools();
 
   /**
-   * A helper method to calculate RMSE.
-   */
-  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
-                                const std::vector<Eigen::VectorXd> &ground_truth);
+  * A helper method to calculate RMSE.
+  */
+  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
 
   /**
-   * A helper method to calculate Jacobians.
+  * A helper method to calculate Jacobians.
+  */
+  MatrixXd CalculateJacobian(const VectorXd& x_state);
+
+  /**
+   * Convert from Cartesian to Polar coordinates.
    */
-  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
+  VectorXd ConvertFromCartesianToPolarCoords(const VectorXd &v_cart);
 
 };
 
-#endif  // TOOLS_H_
+#endif /* TOOLS_H_ */
